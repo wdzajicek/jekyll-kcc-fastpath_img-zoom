0556114b8a63787b7adc2cb465ef0251c8ec1115
wdzajicek <tig.welder4@gmail.com>
2018-01-09 15:16:14 -0600

    Duplicate current passing site build into docs folder for github-pages to fix broken main.min.css link

---
+++ docs/docs/assets/js/script.js
@@ -0,0 +1,10 @@
+// Lazy load images
+// ex. <img data-src="/path/to/image.jpg" alt="">
+(function() {
+  [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = function() {
+      img.removeAttribute('data-src');
+    };
+  })
+})();
